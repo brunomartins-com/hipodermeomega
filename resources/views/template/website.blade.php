54a7dd6772b8155fa8f8f39454abbf0a3a2b0df7
brunomartinsdotcom <hello@brunomartins.com>
2015-11-11 10:41:10 -0500

    Final Adjustes

--- resources/views/template/website.blade.php
+++ resources/views/template/website.blade.php
@@ -2,15 +2,20 @@
 {!! view('website.teaser')->with(compact('page', 'websiteSettings')) !!}
 {{ die }}
 @endif
+<!--
+Project: Bebê Hipoderme Ômega
+Author: Bruno Martins - Web Developer
+Website: www.brunomartins.com
+Contact: hello@brunomartins.com
+-->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="format-detection" content="telephone=no" />
-    <meta name="author" content="Bruno Martins">
     <title>@yield('title'){{ $websiteSettings['title'] }}</title>
-    <link href="{!! asset('assets/images/dados-do-site/'.$websiteSettings['favicon']) !!}" rel="shortcut icon" />
-    <link href="{{ asset('assets/images/dados-do-site/'.$websiteSettings['appleTouchIcon']) }}" rel="apple-touch-icon" />
+    <link href="{!! asset('assets/images/_upload/dados-do-site/'.$websiteSettings['favicon']) !!}" rel="shortcut icon" />
+    <link href="{{ asset('assets/images/_upload/dados-do-site/'.$websiteSettings['appleTouchIcon']) }}" rel="apple-touch-icon" />
     <link href="{!! asset('assets/css/main.css') !!}" rel="stylesheet" type="text/css" />
     {!! $websiteSettings['googleAnalytics'] !!}
     <!-- Meta Tags -->
@@ -20,7 +25,7 @@
     <meta property="og:type" content="website" />
     <meta property="og:locale" content="pt_BR" />
     <meta property="og:url" content="{{ \URL::current() }}" />
-    <meta property="og:image" content="{{ asset('assets/images/dados-do-site/'.$websiteSettings['avatar']) }}" />
+    <meta property="og:image" content="{{ asset('assets/images/_upload/dados-do-site/'.$websiteSettings['avatar']) }}" />
     <meta property="og:site_name" content="@yield('title'){{ $websiteSettings['title'] }}" />
 
     @yield('head')
